df922cb5bdc07a9d70fb4884cad7f291b2b2b665
CosmaxC <491217737@qq.com>
2025-12-29 21:52:36 +0800

    remove base line

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://rrainy5251-hue.github.io",
-  base: "/Rrainy_portfolio/",
   trailingSlash: "always"
 });
