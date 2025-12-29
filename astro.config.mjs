eb71d4695a3f175b22fc9108d6ac6705ae37cd62
CosmaxC <491217737@qq.com>
2025-12-29 21:55:24 +0800

    fix css 404 issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://rrainy5251-hue.github.io",
+  base: "/Rrainy_portfolio/",
   trailingSlash: "always"
 });
