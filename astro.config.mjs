2944b53e252f4aba3b2e717b7181c4ab003cf86c
CosmaxC <491217737@qq.com>
2025-12-29 21:47:19 +0800

    correct the Astro.config for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from "astro/config";
+
 export default defineConfig({
+  site: "https://rrainy5251-hue.github.io",
+  base: "/Rrainy_portfolio/",
   trailingSlash: "always"
 });
